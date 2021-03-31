4a5cb4973d353a721c451991e4981c1dde6a793d
Nazim <halilov.nazim72@gmail.com>
2021-03-31 14:50:43 +0300

    change js and add css-code

--- js/main.js
+++ js/main.js
@@ -1,21 +1,39 @@
 const products = [
-    {id: 1, title: 'Notebook', price: 2000},
-    {id: 2, title: 'Mouse', price: 20},
-    {id: 3, title: 'Keyboard', price: 200},
-    {id: 4, title: 'Gamepad', price: 50},
+    {id: 1, title: 'Notebook', price: 2000, img: 'img/notebook.jpg'},
+    {id: 2, title: 'Mouse', price: 20, img: 'img/mouse.jpg'},
+    {id: 3, title: 'Keyboard', price: 200, img: 'img/keyboard.jpg'},
+    {id: 4, title: 'Gamepad', price: 50, img: 'img/gamepad.jpg'},
 ];
 //Функция для формирования верстки каждого товара
-const renderProduct = (title, price) => {
-    return `<div class="product-item">
-                <h3>${title}</h3>
-                <p>${price}</p>
+const renderProduct = (products) => {
+    return `<div class="products__item">
+                <img class="products__pic" src="${products.img}" alt="${products.title}" width="250">
+                <h3 class="products__title">${products.title}</h3>
+                <p class="products__price">${products.price}$</p>
                 <button class="buy-btn">Купить</button>
             </div>`
 };
 const renderPage = list => {
-    const productsList = list.map(item => renderProduct(item.title, item.price));
+    const productsList = list.map(item => renderProduct(item));
     console.log(productsList);
-    document.querySelector('.products').innerHTML = productsList;
+    document.querySelector('.products').innerHTML = productsList.join('');
 };
 
 renderPage(products);
+
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+
+    let dropdowns = document.getElementsByClassName("dropdown-content");
+    for (let i = 0; i < dropdowns.length; i++) {
+      let openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
